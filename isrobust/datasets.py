bbadb3fe7b7797b04109e9b522d7b18459adaa7c
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2024-01-06 05:54:02 +0000

    Include ivae_scorer modules.

---
+++ isrobust/datasets.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+import scanpy as sc
+
+
+def load_kang(data_folder=".", normalize=True, n_genes=None):
+    data_folder = Path(data_folder)
+    adata = sc.read(
+        data_folder.joinpath("kang_counts_25k.h5ad"),
+        backup_url="https://figshare.com/ndownloader/files/34464122",
+        cache=True,
+    )
+
+    adata.obs["label"] = adata.obs["label"].replace(
+        {"ctrl": "control", "stim": "stimulated"}
+    )
+    adata.obs = adata.obs.rename(columns={"label": "condition"})
+
+    # Storing the counts for later use
+    adata.layers["counts"] = adata.X.copy()
+
+    # Normalizing
+    if normalize:
+        sc.pp.normalize_total(adata)
+        sc.pp.log1p(adata)
+
+    if n_genes is not None:
+        sc.pp.highly_variable_genes(
+            adata, n_top_genes=n_genes, flavor="seurat_v3", subset=False, layer="counts"
+        )
+
+        adata = adata[:, adata.var.highly_variable]
+
+    return adata
