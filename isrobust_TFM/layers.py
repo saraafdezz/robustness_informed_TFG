18d8b2aaedffe854d31a27aadbf0104e887bdc1d
albertoem77 <estebanm.alberto@gmail.com>
2024-10-10 13:23:25 +0200

    Updated function to keras 3

--- isrobust_TFM/layers.py
+++ isrobust_TFM/layers.py
@@ -1,134 +1,71 @@
+import keras
 import numpy as np
 import tensorflow as tf
-import keras
-
-
-@tf.keras.utils.register_keras_serializable()
-class Informed(keras.layers.Dense):
-    def __init__(self, adj, **kwargs):
-        """Informed layer using the Keras API. An Informed layer is defined by an adjacency
-        matrix that indicates which inputs from the previous layer are going to be set to 0.
-        This defines the layer's Kernel.
-
-        Args:
-            adj (n_inputs, n_ouputs): Adjacency matrix to inform the layer kernel.
-        """
-
-        self.adj = adj
-        if adj is not None:
-            units = adj.shape[1]
-        super(Informed, self).__init__(units=units, **kwargs)
-
-    def build(self, input_shape):
-        """Build the layer once its parameters are known.
-
-        Args:
-            input_shape (_type_): Layer  dimensions.
-        """
-
-        connection_vector = 1 * (np.sum(self.adj, axis=0) > 0)
-
-        self.kernel = self.add_weight(
-            shape=(input_shape[-1], self.units),
-            initializer=InformedInitializer(self.adj, self.kernel_initializer),
-            name="kernel",
-            regularizer=self.kernel_regularizer,
-            constraint=self.kernel_constraint,
-        )
-
-        self.kernel_adj = self.add_weight(
-            shape=(input_shape[-1], self.units),
-            initializer=keras.initializers.Constant(self.adj),
-            name="kernel_adj",
-            trainable=False,
-        )
-
-        self.bias = self.add_weight(
-            shape=(self.units,),
-            initializer=InformedInitializer(connection_vector, self.bias_initializer),
-            name="bias",
-            regularizer=self.bias_regularizer,
-            constraint=self.bias_constraint,
-        )
-
-        self.bias_adj = self.add_weight(
-            shape=(self.units,),
-            initializer=keras.initializers.Constant(connection_vector),
-            name="bias_adj",
-            trainable=False,
-        )
-
-        self.built = True
-
-    def call(self, inputs):
-        """Forward pass."""
-
-        output = keras.backend.dot(inputs, self.kernel * self.kernel_adj)
-        output = keras.backend.bias_add(output, self.bias_adj * self.bias)
-        if self.activation is not None:
-            output = self.activation(output)
-        return output
-
-    def count_params(self):
-        """Redefine the number of parameters by taking into account the
-        induced sparsity.
-        """
-
-        n_bias_weights = self.bias_adj.numpy().sum()
-        n_kernel_weights = self.kernel_adj.numpy().sum()
-
-        return n_bias_weights + n_kernel_weights
-
-    def get_config(self):
-        """We need this to make it serializable."""
-
-        config = {"adj": self.adj.tolist()}
-        base_config = super().get_config()
-        base_config.pop("units", None)
-        return dict(list(base_config.items()) + list(config.items()))
-
-    @classmethod
-    def from_config(cls, config):
-        """We need this to make it serializable."""
-
-        adj_as_list = config["adj"]
-        config["adj"] = np.array(adj_as_list)
-        return cls(**config)
-
-
-@tf.keras.utils.register_keras_serializable()
-class InformedInitializer(keras.initializers.Initializer):
-    def __init__(self, adj, initializer):
-        """Propagate the sparsity induced by the adjacency matrix `adj` to
-        the provided `initializer`.
-
-        Args:
-            adj (_type_): Adjacency matrix.
-            initializer (_type_): Keras initalizer.
-        """
-
-        self.adj = adj
-        self.initializer = initializer
-
-    def __call__(self, shape, dtype=None):
-        """Sparsity induction."""
-
-        return tf.constant(self.adj, dtype=dtype) * self.initializer(shape, dtype=dtype)
-
-    def get_config(self):
-        """We need this to make it serializable."""
-
-        return {"adj": self.adj, "initializer": self.initializer}
-
-
 
 class InformedConstraint(keras.constraints.Constraint):
     def __init__(self, adj):
         self.adj = adj
- 
-    def __call__(self,w):
-    
+
+    def __call__(self, w):
         return w * tf.constant(self.adj, dtype=w.dtype)
 
     def get_config(self):
-        return {'adj': self.adj.tolist()}
+        return {"adj": self.adj.tolist()}
+
+class InformedBiasConstraint(keras.constraints.Constraint):
+    def __init__(self, adj):
+        self.adj = 1*(np.sum(adj, axis=0) > 0)
+
+    def __call__(self, w):
+        return w * tf.constant(self.adj, dtype=w.dtype)
+
+    def get_config(self):
+        return {"adj": self.adj.tolist()}
+
+
+
+
+# class InformedInitializer(keras.initializers.Initializer):
+#     def __init__(self, adj, initializer):
+#         self.adj = adj
+#         self.initializer = initializer
+
+#     def __call__(self, shape, dtype=None):
+#         return tf.constant(self.adj, dtype=dtype) * self.initializer(shape, dtype=dtype)
+
+#     def get_config(self):
+#         return {"adj": self.adj, "initializer": self.initializer}
+# class InformedInitializer(keras.initializers.Initializer):
+#     def __init__(self, adj, base_initializer=keras.initializers.GlorotUniform()):
+#         self.adj = adj
+#         self.base_initializer = base_initializer
+
+#     def __call__(self, shape, dtype=None):
+#         base_values = self.base_initializer(shape, dtype)
+#         return tf.where(tf.constant(self.adj, dtype=tf.bool), base_values, tf.zeros_like(base_values))
+
+#     def get_config(self):
+#         return {
+#             'adj': self.adj,
+#             'base_initializer': keras.initializers.serialize(self.base_initializer)
+#         }
+
+#     @classmethod
+#     def from_config(cls, config):
+#         config['base_initializer'] = keras.initializers.deserialize(config['base_initializer'])
+#         return cls(**config)
+
+# class BiasInitializer(keras.initializers.Initializer):
+#     def __init__(self, adj):
+#         self.adj = adj
+
+#     def __call__(self, shape, dtype=None):
+#         bias = 1 * (np.sum(self.adj, axis=0) > 0)
+#         return tf.constant(bias, shape=shape, dtype=dtype)
+
+#     def get_config(self):
+#         return {'adj': self.adj}
+
+#     @classmethod
+#     def from_config(cls, config):
+#         return cls(**config)
