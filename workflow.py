f4435780c599c4181cc22016b2e9e45dc4113a56
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2025-03-04 09:46:31 +0000

    Add workflow.py (prefect).

---
+++ workflow.py
@@ -0,0 +1,202 @@
+import itertools
+import os
+import subprocess
+import time
+
+from dotenv import load_dotenv, find_dotenv
+from prefect import flow, task
+from prefect.cache_policies import INPUTS, TASK_SOURCE
+from prefect.futures import wait
+from prefect.task_runners import ThreadPoolTaskRunner
+from prefect_shell import ShellOperation
+
+
+def check_cli_arg_is_bool(arg):
+    """Check if argument is a boolean.
+
+    Parameters
+    ----------
+    arg : str
+        Argument.
+
+    Returns
+    -------
+    bool
+        Argument.
+    """
+    print("*" * 20, arg)
+    if arg in ["true", "True", "TRUE", "1"]:
+        arg = True
+    elif arg in ["false", "False", "FALSE", "0"]:
+        arg = False
+    else:
+        raise ValueError(f"argument {arg} must be a boolean")
+
+    return arg
+
+
+# --- Load Environment Variables ---
+print("*"*20, find_dotenv())
+load_dotenv()
+
+FRAC_START = os.getenv("FRAC_START", "0.1")
+FRAC_STEP = os.getenv("FRAC_STEP", "0.1")
+FRAC_STOP = os.getenv("FRAC_STOP", "1.0")
+SEED_START = os.getenv("SEED_START", "1")
+SEED_STEP = os.getenv("SEED_STEP", "1")
+SEED_STOP = os.getenv("SEED_STOP", "3")
+RESULTS_FOLDER = os.getenv("RESULTS_FOLDER", "results")
+N_GPU = int(os.getenv("N_GPU", "3"))
+N_CPU = int(os.getenv("N_CPU", "4"))
+DEBUG = check_cli_arg_is_bool(os.getenv("DEBUG", "1"))
+DATA_PATH = os.getenv("DATA_PATH", "data")  # add data path
+
+N_DEVICES = N_GPU if N_GPU > 0 else (N_CPU -1)
+
+print("*" * 20, N_CPU, os.getenv("DEBUG"), DEBUG)
+print("*" * 20, os.getenv("PREFECT_RESULTS_PERSIST_BY_DEFAULT"))
+
+
+# As in the makefile
+# TODO: use a pythonic way to get the list of fractions and seeds
+FRACS = [
+    str(x)
+    for x in subprocess.check_output(
+        f"LANG=en_US seq {FRAC_START} {FRAC_STEP} {FRAC_STOP}", shell=True, text=True
+    )
+    .strip()
+    .split("\n")
+]
+SEEDS = [
+    str(x)
+    for x in subprocess.check_output(
+        f"LANG=en_US seq {SEED_START} {SEED_STEP} {SEED_STOP}", shell=True, text=True
+    )
+    .strip()
+    .split("\n")
+]
+
+
+# --- Tasks ---
+
+
+@task(cache_policy=TASK_SOURCE + INPUTS)
+def install_ivae(results_folder: str = RESULTS_FOLDER):
+    """Installs dependencies using pixi."""
+    os.makedirs(f"{results_folder}/logs", exist_ok=True)
+    ShellOperation(commands=["pixi install -e ivaecuda"], working_dir=".").run()
+
+    return
+
+
+@task(cache_policy=TASK_SOURCE + INPUTS)
+def create_folders(model_type: str, frac: str = None):
+    """Creates folders for a given model type, seed, and optionally fraction."""
+    results_folder = os.path.join(RESULTS_FOLDER, model_type)
+    if frac:
+        results_folder = os.path.join(RESULTS_FOLDER, f"{model_type}-{frac}")
+
+    ShellOperation(commands=[f"mkdir -p {results_folder}/logs"]).run()
+
+    return
+
+
+@task(
+    cache_policy=TASK_SOURCE + (INPUTS - "gpu_id"), retries=3, retry_delay_seconds=2
+) 
+def run_training(model_type: str, seed: str, frac: str = None, gpu_id=None):
+    """Runs the training script for a given model type, seed, and optionally fraction.
+    Which GPU to use is also passed as an argument."""
+    # TODO: adapt to only CPUs scenarios
+
+    results_folder = os.path.join(RESULTS_FOLDER, model_type)
+    if frac:
+        results_folder = os.path.join(
+            RESULTS_FOLDER, f"{model_type}-{frac}"
+        ) 
+
+    command = [
+        "pixi",
+        "run",
+        "--environment",
+        "ivaecuda",
+        "python",
+        "notebooks/00-train.py",
+        "--model_kind",
+        model_type,
+        "--seed",
+        str(seed),
+        "--results_path_model",
+        results_folder,
+        "--data_path",
+        DATA_PATH,
+    ]
+
+    print("*" * 20, DEBUG)
+
+    if DEBUG:
+        command.extend(["--debug", str(DEBUG)])
+    if frac:
+        command.extend(["--frac", str(frac)])
+
+    log_file_out = os.path.join(results_folder, "logs", f"train_seed-{seed}.out")
+    log_file_err = os.path.join(results_folder, "logs", f"train_seed-{seed}.err")
+
+    ShellOperation(
+        commands=[
+            " ".join(command),  # Join command and redirect.
+        ],
+        env={
+            **os.environ,
+            "CUDA_VISIBLE_DEVICES": str(gpu_id),
+        },  # Set CUDA_VISIBLE_DEVICES
+    ).run()
+
+    # create outputs
+    fname = f"metrics-seed-{int(seed):02d}.pkl"
+    if "random" in model_type:
+        return os.path.join(RESULTS_FOLDER, f"{model_type}-{frac}", fname)
+    else:
+        return os.path.join(RESULTS_FOLDER, model_type, fname)
+
+
+# --- Flows ---
+
+
+@flow(
+    name="IVAE Training Workflow", task_runner=ThreadPoolTaskRunner(max_workers=N_DEVICES)
+)  # Give the flow a name
+def main_flow(results_folder: str = RESULTS_FOLDER):
+    """Main workflow to install dependencies and run training for different models."""
+
+    install_ivae(results_folder=results_folder)
+
+    models = [f"ivae_random-{frac}" for frac in FRACS] + ["ivae_kegg", "ivae_reactome"]
+
+    folders = []
+    for model in models:
+        if "random" in model:
+            model_, frac = model.split("-")
+        else:
+            frac = None
+            model_ = model
+        create_folders.submit(model_, frac)
+    wait(folders)
+
+    seeds_run = []
+    for index, (model, seed) in enumerate(itertools.product(models, SEEDS)):
+        if "random" in model:
+            model_, frac = model.split("-")
+        else:
+            frac = None
+            model_ = model
+        seeds_run.append(run_training.submit(model_, seed, frac, gpu_id=index % N_GPU))
+    wait(seeds_run)
+    
+    # Give time to shutdown connections
+    time.sleep(2) 
+
+
+
+if __name__ == "__main__":
+    main_flow()
