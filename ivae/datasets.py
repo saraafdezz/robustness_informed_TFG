b602bc29730543b600fbaf1a4fe1bccf1cb5c376
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2025-04-17 16:14:57 +0000

    Allow to return the path to the processed dataset to avoid spilling data objects.

--- ivae/datasets.py
+++ ivae/datasets.py
@@ -7,8 +7,9 @@ from pathlib import Path
 import scanpy as sc
 
 
-def load_kang(data_folder=".", normalize=True, n_genes=None):
+def load_kang(data_folder=".", normalize=True, n_genes=None, return_path=False):
     data_folder = Path(data_folder)
+    out_path = data_folder.joinpath("kang_counts_25k_for_splitting.h5ad")
     adata = sc.read(
         data_folder.joinpath("kang_counts_25k.h5ad"),
         backup_url="https://figshare.com/ndownloader/files/34464122",
@@ -35,4 +36,9 @@ def load_kang(data_folder=".", normalize=True, n_genes=None):
 
         adata = adata[:, adata.var.highly_variable]
 
-    return adata
+    sc.write(filename=out_path, adata=adata)
+
+    if return_path:
+        return out_path
+    else:
+        return adata
